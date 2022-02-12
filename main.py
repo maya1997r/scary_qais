52b43350c41e1e192e4f0f1268b49a01a2f6676e
maya1997r <83372201+maya1997r@users.noreply.github.com>
2022-02-12 13:07:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import classes
+
+def main():
+
+
+
+    first_doctor = classes.Doctor("Jose" , "bubu" , "nutritionist" , 23)
+    first_patient = classes.Patient("katy" , "kaka" , "+962777071631" , "Jose")
+
+    print(first_doctor.name)
+    print(first_patient.name)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
